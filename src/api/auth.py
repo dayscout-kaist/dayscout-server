9369a0f4451019b7db1ebb391eeb10452052292d
14Kgun <geon6757@kaist.ac.kr>
2023-10-31 00:23:25 +0900

    feat: register_user

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,25 +1,37 @@
-from fastapi import APIRouter
+from typing import Union
 
-from src.services import get_auth_info
+from fastapi import APIRouter
+from starlette.requests import Request
+
+from src.schemas import RegisterBody, UserInfoSession
+from src.services import register_user
 
 router = APIRouter()
 
 
 @router.get("/")
-async def info():
-    return get_auth_info()
+async def info(request: Request) -> Union[UserInfoSession, dict]:
+    userInfo = request.session.get("userInfo")
+    return {} if userInfo == None else userInfo
 
 
 @router.post("/login")
-async def login():
+async def login(request: Request):
+    request.session["userInfo"] = None
     return {}
 
 
 @router.get("/logout")
-async def logout():
-    return {}
+async def logout(request: Request) -> bool:
+    userInfo = request.session.get("userInfo")
+    if userInfo != None:
+        request.session.clear()
+        return True
+    return False
 
 
 @router.post("/register")
-async def logout():
-    return {}
+async def register(body: RegisterBody) -> bool:
+    if register_user(body):
+        return True
+    return False
