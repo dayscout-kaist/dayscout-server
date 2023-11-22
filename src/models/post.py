7dbc147f89623bf23cc60816c0d19563901b4e55
14Kgun <geon6757@kaist.ac.kr>
2023-11-22 17:33:55 +0900

    fix: foreign_key error

--- src/models/post.py
+++ src/models/post.py
@@ -7,8 +7,8 @@ from sqlmodel import JSON, Column, Field, SQLModel
 class PostModel(SQLModel, table=True):
     id: int = Field(primary_key=True, index=True)
     content: str = Field(default=None)
-    food_id: int = Field(default=None, foreign_key="userinfo.id")
-    user_id: int = Field(default=None, foreign_key="foodinfo.id")
+    food_id: int = Field(default=None, foreign_key="usermodel.id")
+    user_id: int = Field(default=None, foreign_key="foodmodel.id")
     tags: List[int] = Field(sa_column=Column(JSON))
     created_at: datetime = Field(default_factory=datetime.utcnow, nullable=False)
 
